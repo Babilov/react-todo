7f4b8d3d65c7a76802e32326620ebcc76b33b4ed
Babilov <egor-kun2017@mail.ru>
2023-09-25 17:17:09 +0300

    pagination

--- todo/src/components/UI/Button/ChangeTodoButton.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { BsPencil } from "react-icons/bs";
-import cl from "./ButtonStyles/ChangeTodoButton.module.css";
-
-export const ChangeTodoButton = ({ onClick }) => {
-  return <BsPencil onClick={onClick} className={cl.changeButton} />;
-};
--- todo/src/utils/getCursor.js
+++
@@ -1,3 +0,0 @@
-export const getCursor = (action) => {
-    return ([action.payload.event.target.selectionStart, action.payload.event.nativeEvent.inputType])
-}
